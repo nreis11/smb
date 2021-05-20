70b1a53656fd629f03a98ca9a038b74950b5f4ea
nreis11 <nicholasreis@icloud.com>
2021-05-20 10:58:13 -0700

    ep14 - dashboard & fonts

--- public/js/loaders/level.js
+++ public/js/loaders/level.js
@@ -1,7 +1,8 @@
 import { Matrix } from "../math.js";
 import Level from "../Level.js";
 import { loadJSON, loadSpriteSheet } from "../loaders.js";
-import { createBackgroundLayer, createSpriteLayer } from "../layers.js";
+import { createSpriteLayer } from "../layers/sprites.js";
+import { createBackgroundLayer } from "../layers/background.js";
 
 function setupCollision(levelSpec, level) {
   const mergedTiles = levelSpec.layers.reduce((mergedTiles, layerSpec) => {
